a9c1cda5ed88ce7d4dd445c47c0396017056bd0d
Anne Patana <apatana@hotmail.com>
2020-05-14 13:00:06 +0200

    Main added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Profile from "./pages/Profile";
+import Main from "./pages/Main";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/signin" component={Signin} />
         <Route exact path="/signup" component={Signup} />
+        <Route exact path="/main" component={Main} />
         <ProtectedRoute exact path="/profile" component={Profile} />
       </Switch>
     </div>
