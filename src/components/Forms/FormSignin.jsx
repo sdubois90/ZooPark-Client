718151cae4b605f2994dcdc08e684d7aafe95a46
YuvalBu <Yuval.Bu@gmail.com>
2020-05-19 17:27:14 +0200

    home, login, signup css

--- src/components/Forms/FormSignin.jsx
+++ src/components/Forms/FormSignin.jsx
@@ -1,57 +1,67 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import UserContext from "../Auth/UserContext";
-import { withRouter } from "react-router-dom";
-import apiHandler from "../../api/apiHandler";
+import UserContext from '../Auth/UserContext';
+import { withRouter } from 'react-router-dom';
+import apiHandler from '../../api/apiHandler';
 
 class FormSignin extends Component {
-  static contextType = UserContext;
+	static contextType = UserContext;
 
-  state = {
-    email: "",
-    password: "",
-  };
+	state = {
+		email: '',
+		password: ''
+	};
 
-  handleChange = (event) => {
-    const key = event.target.name;
+	handleChange = (event) => {
+		const key = event.target.name;
 
-    // You can test more if you have to handle different sorts of inputs.
-    const value =
-      event.target.type === "file"
-        ? event.target.files[0]
-        : event.target.type === "checkbox"
-        ? event.target.checked
-        : event.target.value;
+		// You can test more if you have to handle different sorts of inputs.
+		const value =
+			event.target.type === 'file'
+				? event.target.files[0]
+				: event.target.type === 'checkbox' ? event.target.checked : event.target.value;
 
-    this.setState({ [key]: value });
-  };
+		this.setState({ [key]: value });
+	};
 
-  handleSubmit = (event) => {
-    event.preventDefault();
+	handleSubmit = (event) => {
+		event.preventDefault();
 
-    apiHandler
-      .signin(this.state)
-      .then((data) => {
-        this.context.setUser(data);
-        this.props.history.push("/main");
-      })
-      .catch((error) => {
-        console.log(error);
-        // Display error message here, if you set the state
-      });
-  };
+		apiHandler
+			.signin(this.state)
+			.then((data) => {
+				this.context.setUser(data);
+				this.props.history.push('/main');
+			})
+			.catch((error) => {
+				console.log(error);
+				// Display error message here, if you set the state
+			});
+	};
 
-  render() {
-    return (
-      <form onChange={this.handleChange} onSubmit={this.handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
-        <button>Submit</button>
-      </form>
-    );
-  }
+	render() {
+		return (
+			<div className="home-container" style={{ height: 'calc(100vh - 70px)' }}>
+				<div class="ui center aligned container">
+					<p id="sub-title">Sorry, I can't! My human and I have plans...</p>
+				</div>
+				<form class="ui form text container segment" onChange={this.handleChange} onSubmit={this.handleSubmit}>
+					<div class="required field">
+						<label htmlFor="email">Email</label>
+						<input type="email" name="email" placeholder="meow@schmoe.com" />
+					</div>
+					<div class="required field">
+						<label htmlFor="password">Password</label>
+						<input type="password" name="password" placeholder="Shhh...don't tell it to anyone!" />
+					</div>
+
+					<button class="ui button" type="submit">
+						Submit
+					</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default withRouter(FormSignin);
--- src/components/NavMain.jsx
+++ src/components/NavMain.jsx
@@ -1,55 +1,59 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { withUser } from "../components/Auth/withUser";
-import apiHandler from "../api/apiHandler";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { withUser } from '../components/Auth/withUser';
+import apiHandler from '../api/apiHandler';
 
-import "../styles/NavMain.css";
+import '../styles/NavMain.css';
 
 const NavMain = (props) => {
-  const { context } = props;
+	const { context } = props;
 
-  function handleLogout() {
-    apiHandler
-      .logout()
-      .then(() => {
-        context.removeUser();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+	function handleLogout() {
+		apiHandler
+			.logout()
+			.then(() => {
+				context.removeUser();
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
-  return (
-    <nav className="NavMain">
-      <NavLink exact to="/">
-        <h3 className="logo">App name</h3>
-      </NavLink>
-      <ul className="nav-list">
-        {context.isLoggedIn && (
-          <React.Fragment>
-            <li>
-              <NavLink to="/profile">
-                {context.user && context.user.email}
-              </NavLink>
-            </li>
-            <li>
-              <p onClick={handleLogout}>Logout</p>
-            </li>
-          </React.Fragment>
-        )}
-        {!context.isLoggedIn && (
-          <React.Fragment>
-            <li>
-              <NavLink to="/signin">Log in</NavLink>
-            </li>
-            <li>
-              <NavLink to="/signup">Create account</NavLink>
-            </li>
-          </React.Fragment>
-        )}
-      </ul>
-    </nav>
-  );
+	return (
+		<nav className="NavMain">
+			<NavLink exact to="/">
+				<i className="logo" class="home big black icon" />
+			</NavLink>
+			<ul className="nav-list">
+				{context.isLoggedIn && (
+					<React.Fragment>
+						<li>
+							<NavLink to="/profile" class="ui black button">
+								{context.user && context.user.email}
+							</NavLink>
+						</li>
+						<li>
+							<p onClick={handleLogout}>Logout</p>
+						</li>
+					</React.Fragment>
+				)}
+				{!context.isLoggedIn && (
+					<React.Fragment>
+						<li>
+							<button class="ui black button">
+								<NavLink to="/signin">Log in</NavLink>
+							</button>
+						</li>
+						<li>
+							<button class="ui black button">
+								<NavLink to="/signup">Create account</NavLink>
+							</button>
+						</li>
+					</React.Fragment>
+				)}
+			</ul>
+		</nav>
+	);
 };
 
 export default withUser(NavMain);
