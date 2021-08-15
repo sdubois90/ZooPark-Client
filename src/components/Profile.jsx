9a45b21297c6b2d0dcf858575df936ea05a261ff
sdubois90 <59020477+sdubois90@users.noreply.github.com>
2021-08-15 14:54:08 +0200

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,8 +3,14 @@ import axios from "axios";
 import { withRouter } from 'react-router-dom';
 import { withUser } from "../components/Auth/withUser";
 
+// ajout
+import UserContext from '../Auth/UserContext';
+
 
 class Profile extends Component {
+    // ajout
+    static contextType = UserContext;
+
     constructor (props) {
         super(props);
         this.state = {
@@ -15,7 +21,7 @@ class Profile extends Component {
     componentDidMount() {
         console.log(this.props)
         axios
-            .get(`${process.env.REACT_APP_BACKEND_URL}/api/users/${this.state.currentUser.id}`, {withCredentials: true})
+            .get(`${process.env.REACT_APP_BACKEND_URL}/api/users/${this.context.user._id}`, {withCredentials: true})
             .then((apiResponse) => {
                 console.log(apiResponse);
                 this.setState({ currentUser: apiResponse.data });
@@ -29,10 +35,16 @@ class Profile extends Component {
         return (
             <div>
                 {/* <pre>{JSON.stringify(this.state, null, 2)}</pre> */}
-                <p>{this.state.currentUser._id}</p>
+                {/* <p>{this.state.currentUser._id}</p>
                 <p>{this.state.currentUser.firstName}</p>
                 <p>{this.state.currentUser.lastName}</p>
-                <p>{this.state.currentUser.email}</p>
+                <p>{this.state.currentUser.email}</p> */}
+
+                {/* Ajout */}
+                <p>{this.context.user.firstName}</p>
+                <p>{this.context.user.lastName}</p>
+                <p>{this.context.user.email}</p>
+                
             </div>
         )
     }
